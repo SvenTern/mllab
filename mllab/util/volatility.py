ebfdfe98c787bb2f57e0a17a68fba3550cfa8853
SvenTern <sventern314@hotmail.com>
2024-12-22 04:54:38 -0300

    try 11

--- mllab/util/volatility.py
+++ mllab/util/volatility.py
@@ -33,7 +33,7 @@ def get_daily_vol(close, lookback=50, normalized_data: bool = False):
     if normalized_data:
         returns = close
     else:
-        returns = close.pct_change().ewm(span=lookback).std()
+        returns = close.pct_change()
 
     # Compute the exponentially weighted moving standard deviation
     daily_vol = returns.ewm(span=lookback).std()
