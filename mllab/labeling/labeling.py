c889da2bdc89d6f7904dadea6565227488818e1d
SvenTern <sventern314@hotmail.com>
2025-01-10 10:44:04 -0300

    try 11

--- mllab/labeling/labeling.py
+++ mllab/labeling/labeling.py
@@ -515,7 +515,7 @@ def check_trend_labels_with_period_length(data: pd.DataFrame, labels: pd.DataFra
                     )
             # нужно передвигать период, только если сменился тренд
             if period_length == 1:
-                previous_close = tic_data.loc[idx + short_period + period_length]['close']
+                previous_close = tic_data.loc[idx + short_period -1 + period_length]['close']
                 #previous_end_time = period_end_time
 
     return discrepancies
